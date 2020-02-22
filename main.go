7c7f73ef7c4a4b56af26236814ad467e1e521dc1
TotallyNotMay <kotenkobogdan13@gmail.com>
2020-02-22 18:30:33 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "strings"
+)
+
+func main() {
+  reader := bufio.NewReader(os.Stdin)
+  fmt.Println("Input your expression: ")
+  fmt.Print("~> ")
+  text, _ := reader.ReadString('\n')
+  text = strings.Replace(text, "\n", "", -1)
+  text = strings.Replace(text, "\r", "", -1)
+
+  result, err := PrefixToPostfix(text)
+  fmt.Println("-----------------------------------")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(result)
+}
